2134e33e0d3409ef3528bca2f0b83d5e97ef8425
Dave Reed <dave256@mac.com>
2021-02-03 10:56:17 -0500

    Initial Commit

---
+++ FormDemo/FormDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormDemoApp.swift
+//  FormDemo
+//
+//  Created by David M Reed on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct FormDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
